4d3df64f5113741de81f7a9cd83a1134144952f7
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2026-07-04 14:28:47 +0500

    Generates profile summary after getting information

---
+++ profile_generator.py
@@ -0,0 +1,12 @@
+# Profile generator: Generates profile after getting information.
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+occupation = input("Enter your occupation: ")
+
+print("\n---------- Profile Summary ----------")
+print(f"Name\t\t:\t{name}")
+print(f"Age\t\t:\t{age}")
+print(f"City\t\t:\t{city}")
+print(f"Occupation\t:\t{occupation}")
