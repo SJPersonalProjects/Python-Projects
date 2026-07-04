a1fa01e2f5977946b677df52975133e5e24021c0
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2026-07-04 14:34:01 +0500

    Checks if the number is even or odd.

---
+++ even_odd_checker.py
@@ -0,0 +1,8 @@
+# Checks if the number is even or odd.
+
+number = int(input("Enter number: "))
+
+if number % 2 == 0:
+    print(f"{number} is even number")
+else:
+    print(f"{number} is odd number")
