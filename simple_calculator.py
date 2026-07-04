4d000a7e17904adb8491af2677e72d83ced4409e
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2026-07-04 11:15:43 +0500

    Asks 2 values and calculates.

---
+++ simple_calculator.py
@@ -0,0 +1,12 @@
+# Simple arithmetic calculations.
+
+# Asks 2 values.
+value_one = int(input("Enter value 1: "))
+value_two = int(input("Enter value 2: "))
+
+# Addition.
+print(f"Addition: {value_one + value_two}")
+print(f"Subtraction: {value_one - value_two}")
+print(f"Multiplication: {value_one * value_two}")
+print(f"Division: {value_one / value_two}")
+print(f"Remainder (Modulus): {value_one % value_two}")
