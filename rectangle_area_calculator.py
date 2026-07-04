3a5801a2a04ea2a0f253281d365210f855b8d5aa
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2026-07-04 13:57:16 +0500

    Calculates Area of Rectangle

---
+++ rectangle_area_calculator.py
@@ -0,0 +1,9 @@
+# Calculates area of rectangle.
+length = float(input("Enter length of rectangle: "))
+width = float(input("Enter the width of rectangle: "))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print(f"Area : {area}")
+print(f"Perimeter: {perimeter}")
