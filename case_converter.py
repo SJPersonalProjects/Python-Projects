9cb688423dbae04499f6496c1da28d526bba8413
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2026-07-06 07:29:24 +0500

    Converts case into upper, lower, and title.

---
+++ case_converter.py
@@ -0,0 +1,9 @@
+# Case Converter.
+
+text = input("Enter some text: ")
+
+print("\n=== Case Converter ===")
+print(f"Original: {text}")
+print(f"Upper: {text.upper()}")
+print(f"Lower: {text.lower()}")
+print(f"Title: {text.title()}")
