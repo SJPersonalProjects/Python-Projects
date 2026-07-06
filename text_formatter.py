f980b848d3d3e4aed77fe04def8076d777c63209
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2026-07-06 09:04:45 +0500

    Text formatter

---
+++ text_formatter.py
@@ -0,0 +1,10 @@
+# Text formatter
+
+print("Product".ljust(15) + "Price".rjust(10))
+print("-" * 25)
+
+print("Laptop".ljust(15) + "$800".rjust(10))
+print("Mouse".ljust(15) + "$20".rjust(10))
+print("Keyboard".ljust(15) + "$50".rjust(10))
+
+print("\n" + "Inventory".center(25, "="))
